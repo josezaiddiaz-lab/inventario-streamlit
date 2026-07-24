2cf42ebb5d650e15f01a97664a923bf6e71c3d05
ziad <josezaiddiaz@gmail.com>
2026-07-24 11:29:59 -0600

    Update inv.py

--- inv.py
+++ inv.py
@@ -40,7 +40,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # --------INTERFAZ / CABECERA SUPERIOR---------- 
-col_title, col_menu, col_logo = st.columns([6, 2, 1.2])
+col_title, col_menu, col_logo = st.columns([6, 2.5, 1.2])
 
 with col_title:
     st.title("Control de Inventario GCM")
